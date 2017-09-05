886b6b23f035a49e5c8397f4d2bf0b84877420d3
Tuan Anh Le <anhtuan.0511@gmail.com>
2017-09-05 14:39:34 -0500

    Revert "Test"
    This reverts commit 0f94b71040ad5a9813bda74b87256a41de1b12a7.

--- FPS_Game/Assets/Scripts/GunFire.js
+++
@@ -1,9 +0,0 @@
-﻿function Update () 
-{
-	if(Input.GetButtonDown("Fire1"))
-	{
-		var gunsound : AudioSource = GetComponent.<AudioSource>();
-		gunsound.Play();
-		GetComponent.<Animation>().Play("GunShot");
-	}
-}
