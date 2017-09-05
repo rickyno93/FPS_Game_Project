0f94b71040ad5a9813bda74b87256a41de1b12a7
Tuan Anh Le <anhtuan.0511@gmail.com>
2017-09-05 13:44:29 -0500

    Test

---
+++ FPS_Game/Assets/Scripts/EnemyScript.js
@@ -0,0 +1,14 @@
+﻿var EnemyHealth : int = 30;
+
+function DeductPoints (DamageAmount : int)
+{
+	EnemyHealth -= DamageAmount;
+}
+
+function Update ()
+{
+	if(EnemyHealth <= 0)
+	{
+		Destroy(gameObject);
+	}
+}
